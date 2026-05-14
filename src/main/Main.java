7db1892f85b0255c07c544c508acdd9fcd13a5c6
Jauza Ilham Mahardhika Putra <mahardhika2505@gmail.com>
2026-05-14 14:17:03 +0700

    Added BujurSangkar and Modifed Main for better menu

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,6 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package main;
 
@@ -8,6 +8,8 @@ import java.util.Random;
 import java.util.Scanner;
 import java.util.List;
 import java.util.ArrayList;
+
+import model.BujurSangkar;
 import model.Geometri;
 import model.Persegi;
 import model.LimasPersegi;
@@ -17,7 +19,6 @@ import model.LimasPersegi;
  * @author mahar
  */
 public class Main {
-
     /**
      * @param args the command line arguments
      */
@@ -25,63 +26,110 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
         int jumlahPerhitungan;
+        int pilihanMenu;
+        int tasksPerIteration;
 
-        System.out.println("--uji coba--");
-        System.out.print("Berapa kali perhitungan yang ingin dilakukan = ");
+        do {
+            System.out.println("\n-- Perhitungan Persegi, Limas Persegi, dan Bujur Sangkar --");
+            System.out.println("Pilihan Bangun:");
+            System.out.println("1. Persegi");
+            System.out.println("2. Limas Persegi");
+            System.out.println("3. Bujur Sangkar");
+            System.out.println("4. Kombinasi (semua)");
+            System.out.println("5. exit ");
+            System.out.print("Masukkan Pilihan (1-5) = ");
 
-        try {
-            jumlahPerhitungan = Integer.parseInt(scanner.nextLine());
-        } catch (NumberFormatException e) {
-            System.out.println("Input tidak valid. Menggunakan nilai default (1).");
-            jumlahPerhitungan = 1;
-        }
-
-        System.out.println("\nMemulai " + jumlahPerhitungan + " iterasi perhitungan\n");
-
-        List<Thread> daftarThread = new ArrayList<>();
-        List<HitungTask> daftarTask = new ArrayList<>();
-
-        for (int i = 1; i <= jumlahPerhitungan; i++) {
-            double sisiBangun = random.nextInt(15) + 1;
-            double tinggiBangun = random.nextInt(15) + 1;
-
-            Geometri bangun1 = new Persegi(sisiBangun);
-            Geometri bangun2 = new LimasPersegi(sisiBangun, tinggiBangun);
-
-            String namaPersegi = String.format("Persegi ke-%d dengan sisi=%.0f", i, sisiBangun);
-            String namaLimas = String.format("Limas Persegi ke-%d dengan sisi=%.0f dan tinggi=%.0f", i, sisiBangun, tinggiBangun);
-
-            HitungTask taskPersegi = new HitungTask(bangun1, namaPersegi);
-            HitungTask taskLimas = new HitungTask(bangun2, namaLimas);
-
-            Thread threadPersegi = new Thread(taskPersegi, "Thread-P" + i);
-            Thread threadLimas = new Thread(taskLimas, "Thread-L" + i);
-
-            daftarTask.add(taskPersegi);
-            daftarThread.add(threadPersegi);
-
-            daftarTask.add(taskLimas);
-            daftarThread.add(threadLimas);
-
-            threadPersegi.start();
-            threadLimas.start();
-        }
-        scanner.close();
-
-        for (Thread t : daftarThread) {
             try {
-                t.join();
-            } catch (InterruptedException ex) {
-                System.out.println("Terjadi kesalahan saat menunggu thread");
+                pilihanMenu = Integer.parseInt(scanner.nextLine());
+                if (pilihanMenu == 5) {
+                    System.out.println("Program selesai");
+                    break;
+                }
+                if (pilihanMenu < 1 || pilihanMenu > 5) {
+                    System.out.println("Pilihan tidakk valid. Menggunakan pilihan default (4).");
+                    pilihanMenu = 4;
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Input tidak valid. Menggunakan Pilihan default (4).");
+                pilihanMenu = 4;
             }
-        }
 
-        System.out.println("\nSemua Thread selesai perhitungan");
-        System.out.println("--Hasil Perhitungan--");
+            System.out.print("Berapa kali perhitugan yang ingin dilakukan = ");
+            try {
+                jumlahPerhitungan = Integer.parseInt(scanner.nextLine());
+            } catch (NumberFormatException e) {
+                System.out.println("Input tidak valid. Menggunakan nilai default (1).");
+                jumlahPerhitungan = 1;
+            }
 
-        // 3. Fase Mencetak Hasil Detail secara Berurutan
-        for (HitungTask task : daftarTask) {
-            System.out.println(task.getHasilAkhir());
-        }
+            System.out.println("\nMemulai " + jumlahPerhitungan + " iterasi perhitungan\n");
+
+            List<Thread> daftarThread = new ArrayList<>();
+            List<HitungTask> daftarTask = new ArrayList<>();
+
+            for (int i = 1; i <= jumlahPerhitungan; i++) {
+                double sisiBangun = random.nextInt(15) + 1;
+                double tinggiBangun = random.nextInt(15) + 1;
+
+                if (pilihanMenu == 1 || pilihanMenu == 4) {
+                    Geometri bangun = new Persegi(sisiBangun);
+                    String nama = String.format("Persegi ke-%d dengan sisi= %.0f", i, sisiBangun);
+                    HitungTask task = new HitungTask(bangun, nama);
+                    Thread thread = new Thread(task, "Thread-P" + i);
+
+                    daftarTask.add(task);
+                    daftarThread.add(thread);
+                    thread.start();
+
+                }
+
+                if (pilihanMenu == 2 || pilihanMenu == 4) {
+                    Geometri bangun = new LimasPersegi(sisiBangun, tinggiBangun);
+                    String nama = String.format("Limas Persegi ke-%d dengan sisi= %.0f dan tinggi= %.0f", i, sisiBangun, tinggiBangun);
+                    HitungTask task = new HitungTask(bangun, nama);
+                    Thread thread = new Thread(task, "Thread-L" + i);
+
+                    daftarTask.add(task);
+                    daftarThread.add(thread);
+                    thread.start();
+
+                }
+
+                if (pilihanMenu == 3 || pilihanMenu == 4) {
+                    Geometri bangun = new BujurSangkar(sisiBangun, tinggiBangun);
+                    String nama = String.format("Bujur Sangkar ke-%d dengan sisi= %.0f dan tinggi= %.0f", i, sisiBangun, tinggiBangun);
+                    HitungTask task = new HitungTask(bangun, nama);
+                    Thread thread = new Thread(task, "Thread-B" + i);
+
+                    daftarTask.add(task);
+                    daftarThread.add(thread);
+                    thread.start();
+                }
+            }
+
+            for (Thread t : daftarThread) {
+                try {
+                    t.join();
+                } catch (InterruptedException ex) {
+                    System.out.println("Terjadi kesalahan saat menunggu thread");
+                }
+            }
+
+            System.out.println("\nSemua Thread selesai perhitungan");
+            System.out.println("--Hasil Perhitungan--");
+
+            tasksPerIteration = (pilihanMenu == 4) ? 3 : 1;
+
+            for (int i = 0; i < daftarTask.size(); i++) {
+                System.out.println(daftarTask.get(i).getHasilAkhir());
+
+                if ((i + 1) % tasksPerIteration == 0 && i < daftarTask.size() - 1) {
+                    System.out.println();
+                }
+            }
+        } while (pilihanMenu != 5);
+
+        scanner.close();
     }
+
 }
---
+++ src/model/BujurSangkar.java
@@ -0,0 +1,49 @@
+package model;
+
+public class BujurSangkar extends BangunRuang{
+    private double sisiAlas;
+    private double tinggiPrisma;
+
+    public BujurSangkar(double sisiAlas, double tinggiPrisma) {
+        this.sisiAlas = sisiAlas;
+        this.tinggiPrisma = tinggiPrisma;
+    }
+
+    public double getSisiAlas() {
+        return sisiAlas;
+    }
+
+    public void setSisiAlas(double sisiAlas) {
+        this.sisiAlas = sisiAlas;
+    }
+
+    public double getTinggiPrisma() {
+        return tinggiPrisma;
+    }
+
+    public void setTinggiPrisma(double tinggiPrisma) {
+        this.tinggiPrisma = tinggiPrisma;
+    }
+
+    @Override
+    public double hitungLuas() {
+        double luasAlas = sisiAlas * sisiAlas;
+        double luasDinding = sisiAlas * tinggiPrisma;
+        double luasPermukaan = (2 * luasAlas) + (4 * luasDinding);
+        return luasPermukaan;
+    }
+
+    @Override
+    public double hitungKeliling() {
+        double totalRusukAlasTutup = 8 * sisiAlas;
+        double totalRusukTegak = 4 * tinggiPrisma;
+        double keliling = totalRusukAlasTutup + totalRusukTegak;
+        return keliling;
+    }
+
+    @Override
+    public double hitungVolume() {
+        double volume = (sisiAlas * sisiAlas) * tinggiPrisma;
+        return volume;
+    }
+}
